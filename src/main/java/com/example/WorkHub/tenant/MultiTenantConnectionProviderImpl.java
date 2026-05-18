c99b2a6de9f355a71f3a0f0dab5cf5ec968b91fb
alhusseain <husseainshalaby6@gmail.com>
2026-05-18 03:14:36 +0300

    schema per tenant isolation

---
+++ src/main/java/com/example/WorkHub/tenant/MultiTenantConnectionProviderImpl.java
@@ -0,0 +1,4 @@
+package com.example.WorkHub.tenant;
+
+public class MultiTenantConnectionProviderImpl {
+}
---
+++ src/main/java/com/example/WorkHub/tenant/strategy/DiscriminatorTenancyStrategy.java
@@ -0,0 +1,4 @@
+package com.example.WorkHub.tenant.strategy;
+
+public class DiscriminatorTenancyStrategy {
+}
